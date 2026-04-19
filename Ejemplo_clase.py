b82b7b893e225ad539ed393566380063c2b371d9
José Darío Hilerio Porchini <dario.dragonball.633@gmail.com>
2026-04-18 21:27:05 -0600

    Cree los 5 objetos privados

--- Ejemplo_clase.py
+++ Ejemplo_clase.py
@@ -1,2 +1,9 @@
 class Videojuego:
-    pass
+
+    def _init_(self, titulo, genero, precio, plataforma, horas_jugadas):
+        self.__titulo = titulo
+        self.__genero = genero
+        self.__precio = precio
+        self.__plataforma = plataforma
+        self.__horas_jugadas = horas_jugadas
+
